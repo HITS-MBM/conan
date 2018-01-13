35814eacd8c95a7c6e92529e991f5cd826be216b
Csaba Daday <csaba.daday@h-its.org>
2018-01-13 22:43:17 +0100

    The testing module now looks for gmx, gnuplot, and mencoder. Furthermore, the linkage files are not checked -- they are not unique. The cluster assignment is checked instead.

--- test/test.py
+++ test/test.py
@@ -1,9 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import numpy as np
 import sys
 import os
 
+def which(program):
+# This function checks for the existence of an executable
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return ''
+
+def cleanup():
+  for y in os.listdir("."):
+    if os.path.isdir(y) and y!="correct_output" and y!="to_read":
+      os.system("rm -r %s"%y)
+
 print ("Welcome to the CONAN test script. Fingers crossed!")
+for prog in ["gmx", "mencoder", "gnuplot"]:
+  print ("Checking for %s..."%prog, end = '')
+  if (which(prog) != ''):
+    print("FOUND")
+  else:
+    print("MISSING")
+    if (prog == "gmx"):
+      print("Without gmx, CONAN can only work using the keyword REREAD. Testing it will not work now. Best of luck!")
+      sys.exit()
+    else:
+      print("This is only used for visualization purposes. We can still test the program, but you are missing out!")
+
 print ("Test 1: I/O, Result: ", end = '')
 os.chdir("1_ReadWrite") # the first test, getting us the actual data.
 os.system("echo 2 | ../../conan.py test.inp > /dev/null >& /dev/null")
@@ -12,7 +45,7 @@ if (i==0):
   print ("PASSED")
 else:
   print ("FAILED")
-
+cleanup()
 i = 1
 tests = []
 os.chdir("..")
@@ -20,6 +53,8 @@ for x in os.listdir("."):
   if os.path.isdir(x) and x!="1_ReadWrite":
     tests.append(x)
 tests.sort()
+total_fails = 0
+failures = []
 for test in tests:
   os.chdir(test)
   i += 1
@@ -31,10 +66,20 @@ for test in tests:
   e = os.system("python ../compare_all.py")
   if (e==0):
     print ("PASSED")
+    os.system("rm dmf.xpm")
+    cleanup()
   else:
     print ("FAILED")
-  os.system("rm dmf.xpm")
-  for y in os.listdir("."):
-    if os.path.isdir(y) and y!="correct_output" and y!="to_read":
-      os.system("rm -r %s"%y)
+    total_fails += 1
+    failures.append(test)
   os.chdir("..")
+
+if (total_fails == 0):
+  print("All tests passed! Everything looks good.")
+  os.system("rm 1_ReadWrite/{dmf.xpm,dm.xpm}")
+else:
+  print("%i test(s) failed. The output of the failed tests has been preserved.")
+  print("It is possible that the deviations are minor.")
+  print("The failures are in the following folders:")
+  for failure in failures:
+    print(failure)
